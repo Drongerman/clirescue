32abf506fefe89e700fa847a50b4c08654941d5d
Andrey S <andreyshalunoff@gmail.com>
2022-02-20 15:38:55 +0400

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/Drongerman/clirescue/cmdutil"
+	"github.com/Drongerman/clirescue/user"
 )
 
 var (
